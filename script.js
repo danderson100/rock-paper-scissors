09a29a4020da44cb69bef594b22f626aa83eb135
danderson100 <danderson7790@gmail.com>
2021-01-06 11:04:43 -0700

    Updated UI - finished

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+             
+    const playBtn = document.querySelector('#play-button');
+    playBtn.addEventListener('click', choose);
+
+    const audioOffBtn = document.querySelector('#audio-button');
+    audioOffBtn.addEventListener('click', audioOff);
+
+    let userPoints = document.querySelector('#user-score');
+    let compPoints = document.querySelector('#comp-score');
+
+    let gameStatus = document.querySelector('.game-status');
+    
+
+    let playerScore = 0;
+    let compScore = 0;
+      
+      
       //this function generates randomly Rock, Paper, or Scissors
        //@return moves[index], which is the random move.
        function computerPlay() {
@@ -15,6 +32,7 @@
     }
 
     function choose() {
+
 
         //play Mortal Combat intro
         const audio = document.querySelector('#mc-start');
@@ -37,8 +55,10 @@
         var reply_click = function()
         {
             console.log("Button clicked, id "+this.id+", text"+this.innerHTML);
-
-            game(this.id);
+            //starts game
+                game(this.id);
+            
+            
         }
         document.getElementById('rock').onclick = reply_click;
         document.getElementById('paper').onclick = reply_click;
@@ -47,38 +67,57 @@
     }
        
        function game(playerSelection) {
+           if (noWinner()) {
 
-           let playerScore = 0;
-           let compScore = 0;
-       
-           
            let resultStr = "";
         
            let computerSelection = computerPlay();
-
-           const img = document.getElementById(playerSelection + '-img');
-           img.classList.toggle('hidden');
-           setTimeout(function(){ console.log("setTimeout"); 
-                img.classList.toggle('hidden');}, 3300);
-           
                
               resultStr = playRound(playerSelection, computerSelection);
               console.log(resultStr);
                
               if (resultStr.includes("win")) {
                   playerScore += 1;
+                  userPoints.textContent = playerScore;
               } else if (resultStr.includes("lose")) {
                   compScore += 1;
+                  compPoints.textContent = compScore;
               }
-           
-           if (playerScore > compScore) {
-              console.log("You win!"); 
-           } else if (compScore > playerScore) {
-              console.log("You lose.");  
-           } else {
-              console.log("You tied!"); 
+
+
+              if (playerScore == 5) {
+                  gameStatus.textContent = "YOU WIN!!!";
+                  resetGame();
+              } else if (compScore == 5) {
+                  gameStatus.textContent = "YOU LOST!!!";
+                  resetGame();
+              } else {
+                  gameStatus.textContent = resultStr;
+              }
+              
            }
            
+       }
+
+       function noWinner() {
+           if (playerScore == 5 || compScore == 5) {
+                return false;
+           } else {
+               return true;
+           }
+       }
+
+       function resetGame() {
+           console.log("Resetting...");
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach((button) => {
+            button.classList.toggle('hidden');
+        });
+            playBtn.setAttribute('style', 'width: 450px; font-size: 70px');
+            playBtn.textContent = "Play Again";
+            playBtn.addEventListener('click', () => window.location.reload());
+
+
        }
        
        function playRound(playerSelection, computerSelection) {
@@ -156,9 +195,3 @@
            
            return resultStr;
        }
-       
-       const playBtn = document.querySelector('#play-button');
-       playBtn.addEventListener('click', choose);
-
-       const audioOffBtn = document.querySelector('#audio-button');
-       audioOffBtn.addEventListener('click', audioOff);
